a1f247dc835e43032e0caf10ce5e3d865090b18e
Nicolas Garcia Gomez <162913631+ngg1017@users.noreply.github.com>
2026-01-20 12:39:43 +0100

    Actualizacion final
    Actualizo los archivos que había subido basando mi proyecto en pyspark pasándolos a pandas

--- Programa.py
+++ Programa.py
@@ -1,23 +1,48 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-import os
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
 
-os.environ["JAVA_HOME"] = "C:/Program Files/Java/jdk-21" 
-os.environ["HADOOP_HOME"] = "C:/hadoop"
-spark = SparkSession.builder \
-        .appName('primeros_pasos') \
-        .master("local[*]") \
-        .config("spark.driver.host", "localhost") \
-        .getOrCreate()
+ventada = tk.Tk()
+ventada.withdraw()
+ventada.attributes('-topmost', True) # Esto asegura que la ventana salga al frente
 
-sc = spark.sparkContext
+ruta_seleccionada = filedialog.askopenfilename(
+    title = "Selecciona un archivo CSV",
+    filetypes = (("Archivos CSV", "*.csv"), ("Todos los archivos", "*.*")))
 
-def funcion_secundaria():
-    df_doctor = spark.read.format ("csv").option("header", "true").load("Practica_Provisionales.csv")
-    df_doctor.show(1)
+if ruta_seleccionada:
+    tipo_archivo = ruta_seleccionada.split(".")[1]
+    if tipo_archivo != "csv":
+        tipo_archivo = None
+        print("El archivo solo puede ser del tipo CSV")
+    else:
+        print("Perfecto, ruta seleccionada correctamente")
+
+else:
+    print("Ruta seleccionada incorrectamente")
+
+ventada.destroy()
+df_principal = pd.read_csv(ruta_seleccionada, na_values = [""])
+
+def codigo_apache(apache):
+    if pd.isna(apache): return 0
+    elif apache <= 4: return 0.04
+    elif apache <= 9: return 0.08
+    elif apache <= 14: return 0.15
+    elif apache <= 19: return 0.25
+    elif apache <= 24: return 0.40
+    elif apache <= 29: return 0.55
+    elif apache <= 34: return 0.75
+    else: return 0.85
+
+def mortalidad_estandarizada():
+    numero_fallecidos = (len(df_principal[df_principal["Fallecido"] == True]))
+    df_principal["probabilidad_mortalidad"] = df_principal["Apache"].apply(codigo_apache)
+    suma_mortalidad = sum(df_principal["probabilidad_mortalidad"][df_principal["Fallecido"] == True])
+    return numero_fallecidos/ suma_mortalidad
 
 def main():
-    funcion_secundaria()
+    print(mortalidad_estandarizada())
     
 if __name__ == "__main__":
     main()
